35c0d0e32a9ab0e3871d0e88f4a9fb209ac73d4e
Dylan Fritz <dfritz1211@gmail.com>
2025-04-18 18:56:01 -0500

    feat: added main game loop

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from game import Game
-game = Game()
-numHumans = int(input("How many human players do you want to add? "))
-for _ in range(numHumans):
-    playerName = input("Enter the player's name: ")
-    game.addPlayer(playerName, False, False)
+play = "yes"
+while play == "yes":
+    game = Game()
+    numHumans = int(input("How many human players do you want to add? "))
+    for _ in range(numHumans):
+        playerName = input("Enter the player's name: ")
+        game.addPlayer(playerName, False, False)
 
-numRobots = int(input("How many robot players do you want to add? "))
-game.printStatus()
-game.playGame(numRobots)
+    numRobots = int(input("How many robot players do you want to add? "))
+    game.printStatus()
+    game.playGame(numRobots)
+    play = input("Play again? (yes/no): ").lower()
 
 
